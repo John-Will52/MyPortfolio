aa259b28e1721d0ee860ccea31127de0e42ca4e5
John-Will52 <johnwill52is@gmail.com>
2019-10-29 15:22:55 -0700

    Angular-Wannabe Adjustments
    Turned my page into a single-page application

--- apps/softDev/urls.py
+++ apps/softDev/urls.py
@@ -3,7 +3,4 @@ from . import views
                     
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^about$', views.about, name='about'),
-    url(r'^resume$', views.resume, name='resume'),
-    url(r'^workshop$', views.workshop, name='workshop'),
 ]
--- apps/softDev/views.py
+++ apps/softDev/views.py
@@ -4,33 +4,3 @@ from .models import Message
 
 def index(request):
     return render(request, "softDev/Home.html")
-
-def about(request):
-    return render(request, "softDev/AboutMe.html")
-
-def resume(request):
-    return render(request, "softDev/Resume.html")
-
-def workshop(request):
-    return render(request, "softDev/Workshop.html")
-
-# def contact(request):
-#     if request.method == 'GET':
-#         return render(request, "softDev/Contact.html")
-
-#     if request.method == 'POST':
-#         Message.objects.create(name = request.POST['Name'], email = request.POST['Email'], message = request.POST['Message'])
-#         return redirect("/contact")
-        
-# def masterEntry(request):
-#     context = {
-#         "allMessages": Message.objects.all()
-#     }
-#     return render(request, "softDev/BackDoor.html", context)
-        
-#         send_mail('{name}',
-#          '{message}',
-#          '{email}',
-#          ['johnwill52is@gmail.com'],
-#          fail_silently=False)
-#          return redirect("/contact")
